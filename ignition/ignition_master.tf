b5ea22e13474b098c8e8c7e3a3e58ce7b0fbc9f3
Dimitrios Karagiannis <dhkarag@gmail.com>
2017-08-03 16:45:22 +0100

    masters should use apiserver certs

--- ignition/ignition_master.tf
+++ ignition/ignition_master.tf
@@ -2,7 +2,7 @@ data "template_file" "master-get-ssl" {
   template = "${file("${path.module}/resources/get-ssl.service")}"
 
   vars {
-    ssl_tar_url = "s3://${var.ssl_s3_bucket}/certs/k8s-worker.tar"
+    ssl_tar_url = "s3://${var.ssl_s3_bucket}/certs/k8s-apiserver.tar"
   }
 }
 
