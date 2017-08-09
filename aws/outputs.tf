e36f9467b8e8ed42fd408b7a2b8e98810804eb4c
Dimitrios Karagiannis <dhkarag@gmail.com>
2017-08-09 14:06:33 +0100

    adding more required outputs

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -5,3 +5,7 @@ output "etcd_ip_list" {
 output "master_address" {
   value = "${aws_route53_record.master-elb.name}"
 }
+
+output "worker_security_group_id" {
+  value = "${aws_security_group.worker.id}"
+}
