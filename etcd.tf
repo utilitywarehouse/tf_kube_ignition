31d70947704442200bbc859275b27dbdadee59ef
Dimitrios Karagiannis <dhkarag@gmail.com>
2017-10-03 17:57:08 +0100

    comments

--- etcd.tf
+++ etcd.tf
@@ -130,6 +130,18 @@ data "template_file" "etcd-metrics-proxy" {
   }
 }
 
+#
+# This is a simple go app that exposes the metrics endpoint of etcd on a
+# non-authenticated port, to avoid having to pass certificates & keys to
+# prometheus. There's a change on etcd that achieves the same without
+# the need for an additional service but it's scheduled for version 3.3.
+# Until then, we will use this helper service.
+#
+# NOTE: This service essentially has full access on the data stored in etcd.
+# The docker image is built using automated builds in quay.io out from an open
+# GitHub repository. If you have reservations, you can always fork the
+# repository and build your own images (like we've done).
+#
 data "ignition_systemd_unit" "etcd-metrics-proxy" {
   count = "${length(var.etcd_addresses)}"
   name  = "etcd-metrics-proxy.service"
