b91800e2efc5f5a8878611c9705f3d8fb811ad96
Foivos Filippopoulos <ffilippopoulos@utilitywarehouse.co.uk>
2018-02-19 12:26:20 +0000

    Check that we received a valid key from cfssl in sk-get

--- resources/cfssl-sk-get.sh
+++ resources/cfssl-sk-get.sh
@@ -9,4 +9,14 @@ cd ${path}
     -H 'Authorization: Basic ${auth}' \
     http://$(jq -r '.remotes.server | split(":")[0]' /etc/cfssl/config.json):8889/signing-key
 
+# Check that we got a valid ec key
+set +e
+/usr/bin/openssl ec -in ${path}/signing-key.pem -noout
+if [ $? -ne 0 ]; then
+	echo "Failed to get EC key from cfssl server";
+	exit 1;
+fi
+set -e
+
+# Give the right permissions
 /usr/bin/chmod 0600 signing-key.pem
