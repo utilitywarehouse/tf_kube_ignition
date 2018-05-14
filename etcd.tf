c153117cb28dc00351fd97177e49d3727c09e172
Hector Huertas <hectorhuertas@gmail.com>
2018-05-14 16:23:52 +0100

    Have etcd use format-and-mount.sh

--- etcd.tf
+++ etcd.tf
@@ -67,52 +67,24 @@ data "ignition_file" "etcdctl-wrapper" {
   }
 }
 
-data "template_file" "disk-formatter" {
-  count = "${length(var.etcd_addresses)}"
-
-  template = "${ var.cloud_provider == "aws" ? 
-	                 file("${path.module}/resources/aws-disk-formatter.service") 
-								:var.cloud_provider == "gce" ?
-								   file("${path.module}/resources/gce-disk-formatter.service")
-								:""
-							}"
+data "template_file" "etcd-disk-mounter" {
+  count    = "${length(var.etcd_addresses)}"
+  template = "${file("${path.module}/resources/disk-mounter.service")}"
 
   vars {
-    volumeid   = "${var.etcd_data_volumeids[count.index]}"
-    user       = "etcd"
-    group      = "etcd"
-    filesystem = "ext4"
+    script_path = "/opt/bin/format-and-mount.sh"
+    volume_id   = "${var.etcd_data_volumeids[count.index]}"
+    filesystem  = "ext4"
+    user        = "etcd"
+    group       = "etcd"
+    mountpoint  = "/var/lib/etcd"
   }
 }
 
-data "ignition_systemd_unit" "disk-formatter" {
+data "ignition_systemd_unit" "etcd-disk-mounter" {
   count   = "${length(var.etcd_addresses)}"
-  name    = "disk-formatter.service"
-  content = "${element(data.template_file.disk-formatter.*.rendered, count.index)}"
-}
-
-data "template_file" "disk-mounter" {
-  count = "${length(var.etcd_addresses)}"
-
-  template = "${ var.cloud_provider == "aws" ?  
-									file("${path.module}/resources/aws-disk-mounter.mount")
-								:var.cloud_provider == "gce" ?
-									file("${path.module}/resources/gce-disk-mounter.mount")
-								:""
-							}"
-
-  vars {
-    volumeid       = "${var.etcd_data_volumeids[count.index]}"
-    mountpoint     = "/var/lib/etcd"
-    filesystem     = "ext4"
-    disk-formatter = "disk-formatter.service"
-  }
-}
-
-data "ignition_systemd_unit" "var-lib-etcd-mounter" {
-  count   = "${length(var.etcd_addresses)}"
-  name    = "var-lib-etcd.mount"
-  content = "${element(data.template_file.disk-mounter.*.rendered, count.index)}"
+  name    = "disk-mounter.service"
+  content = "${data.template_file.etcd-disk-mounter.*.rendered[count.index]}"
 }
 
 resource "null_resource" "etcd_member" {
@@ -164,6 +136,7 @@ data "ignition_config" "etcd" {
         element(data.ignition_file.etcd-cfssl-new-cert.*.id, count.index),
         data.ignition_file.etcd-prom-machine-role.id,
         element(data.ignition_file.etcdctl-wrapper.*.id, count.index),
+        data.ignition_file.format-and-mount.id,
     ),
     var.etcd_additional_files,
   )}"]
@@ -175,8 +148,7 @@ data "ignition_config" "etcd" {
         data.ignition_systemd_unit.docker-opts-dropin.id,
         data.ignition_systemd_unit.node-exporter.id,
         element(data.ignition_systemd_unit.etcd-member-dropin.*.id, count.index),
-        element(data.ignition_systemd_unit.disk-formatter.*.id, count.index),
-        element(data.ignition_systemd_unit.var-lib-etcd-mounter.*.id, count.index),
+        element(data.ignition_systemd_unit.etcd-disk-mounter.*.id, count.index),
     ),
     module.etcd-member-restarter.systemd_units,
     var.etcd_additional_systemd_units,
