a78699af6857f3d6efc968c31a41a974a69e1da8
Hector Huertas <hectorhuertas@gmail.com>
2019-05-01 11:08:00 +0200

    sys: add ro_rootfs metric in a tmpfs folder (#81)

--- worker.tf
+++ worker.tf
@@ -83,16 +83,6 @@ data "ignition_file" "worker-kubeconfig" {
   }
 }
 
-data "ignition_file" "worker-prom-machine-role" {
-  mode       = 0644
-  filesystem = "root"
-  path       = "/etc/prom-text-collectors/machine_role.prom"
-
-  content {
-    content = "machine_role{role=\"worker\"} 1\n"
-  }
-}
-
 data "ignition_file" "worker-sysctl-vm" {
   mode       = 0644
   filesystem = "root"
@@ -103,6 +93,57 @@ data "ignition_file" "worker-sysctl-vm" {
   }
 }
 
+data "template_file" "prometheus-tmpfs-dir" {
+  template = "${file("${path.module}/resources/prometheus-tmpfs-dir.service")}"
+}
+
+data "ignition_systemd_unit" "prometheus-tmpfs-dir" {
+  name    = "prometheus-tmpfs-dir.service"
+  content = "${data.template_file.prometheus-tmpfs-dir.rendered}"
+}
+
+data "template_file" "prometheus-machine-role" {
+  template = "${file("${path.module}/resources/prometheus-machine-role.service")}"
+
+  vars {
+    role = "worker"
+  }
+}
+
+data "ignition_systemd_unit" "prometheus-machine-role" {
+  name    = "prometheus-machine-role.service"
+  content = "${data.template_file.prometheus-machine-role.rendered}"
+}
+
+data "template_file" "prometheus-ro-rootfs" {
+  template = "${file("${path.module}/resources/prometheus-ro-rootfs.service")}"
+}
+
+data "ignition_systemd_unit" "prometheus-ro-rootfs" {
+  name    = "prometheus-ro-rootfs.service"
+  content = "${data.template_file.prometheus-ro-rootfs.rendered}"
+}
+
+data "template_file" "prometheus-ro-rootfs-timer" {
+  template = "${file("${path.module}/resources/prometheus-ro-rootfs.timer")}"
+}
+
+data "ignition_systemd_unit" "prometheus-ro-rootfs-timer" {
+  name    = "prometheus-ro-rootfs.timer"
+  content = "${data.template_file.prometheus-ro-rootfs-timer.rendered}"
+}
+
+data "ignition_file" "prometheus-ro-rootfs" {
+  mode       = 0755
+  filesystem = "root"
+  path       = "/opt/bin/prometheus-ro-rootfs"
+
+  content {
+    content = "${file("${path.module}/resources/prometheus-ro-rootfs")}"
+  }
+}
+
+// data.ignition_file.worker-prom-machine-role.id,
 data "ignition_config" "worker" {
   files = ["${concat(
     list(
@@ -110,10 +151,10 @@ data "ignition_config" "worker" {
         data.ignition_file.cfssljson.id,
         data.ignition_file.cfssl-client-config.id,
         data.ignition_file.worker-cfssl-new-cert.id,
-        data.ignition_file.worker-prom-machine-role.id,
         data.ignition_file.worker-kubeconfig.id,
         data.ignition_file.worker-sysctl-vm.id,
         data.ignition_file.worker-kubelet-conf.id,
+        data.ignition_file.prometheus-ro-rootfs.id,
     ),
     var.worker_additional_files
   )}"]
@@ -124,6 +165,10 @@ data "ignition_config" "worker" {
         data.ignition_systemd_unit.locksmithd_worker.id,
         data.ignition_systemd_unit.docker-opts-dropin.id,
         data.ignition_systemd_unit.worker-kubelet.id,
+        data.ignition_systemd_unit.prometheus-tmpfs-dir.id,
+        data.ignition_systemd_unit.prometheus-machine-role.id,
+        data.ignition_systemd_unit.prometheus-ro-rootfs.id,
+        data.ignition_systemd_unit.prometheus-ro-rootfs-timer.id,
     ),
     module.kubelet-restarter.systemd_units,
     var.worker_additional_systemd_units
