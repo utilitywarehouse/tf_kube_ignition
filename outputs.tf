5c5a0a8089dab0703bee9e8dce326831213ca695
Foivos Filippopoulos <ffilippopoulos@utilitywarehouse.co.uk>
2019-07-10 10:56:44 +0100

    Rewrite storage-worker for terraform >=0.12

--- outputs.tf
+++ outputs.tf
@@ -40,7 +40,7 @@ output "worker_ignition_files" {
 }
 
 output "storage_worker_ignition_systemd" {
-  value = "${data.ignition_config.storage-worker.systemd}"
+  value = data.ignition_config.storage-worker.systemd
 }
 
 output "etcd_ignition_systemd" {
