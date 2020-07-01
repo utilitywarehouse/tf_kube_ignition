c4cc6d08a87450a20f3ed4263a39f6e55d7ba3a1
Foivos Filippopoulos <ffilippopoulos@utilitywarehouse.co.uk>
2020-07-01 13:22:56 +0100

    Update node exporter to v1.0.1

--- variables.tf
+++ variables.tf
@@ -44,7 +44,7 @@ variable "node_exporter_image_url" {
 
 variable "node_exporter_image_tag" {
   description = "The version of the node_exporter image to use."
-  default     = "v0.18.0"
+  default     = "v1.0.1"
 }
 
 variable "hyperkube_image_url" {
