45129e1368c7a535db60d3a16c6f3933270816a7
Rob Best <robertbest89@gmail.com>
2021-01-14 11:25:12 +0000

    Kubernetes 1.20.2

--- variables.tf
+++ variables.tf
@@ -49,7 +49,7 @@ variable "node_exporter_image_tag" {
 
 variable "kubernetes_version" {
   description = "Kubernetes version, used to specify k8s.gcr.io docker image version and Kubernetes binaries"
-  default     = "v1.20.0"
+  default     = "v1.20.2"
 }
 
 variable "cluster_dns" {
