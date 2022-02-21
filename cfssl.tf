6d1092730708209adf196119d03fe174c9831775
George Angel <george-angel@users.noreply.github.com>
2022-02-21 18:47:35 +1000

    Remove fstrim service (#180)
    We have now moved nodes in merit to online discard, no need for periodic
    fstrim.

--- cfssl.tf
+++ cfssl.tf
@@ -217,8 +217,6 @@ data "ignition_config" "cfssl" {
       data.ignition_systemd_unit.cfssl.rendered,
       data.ignition_systemd_unit.containerd-dropin.rendered,
       data.ignition_systemd_unit.docker-opts-dropin.rendered,
-      data.ignition_systemd_unit.fstrim_dropin.rendered,
-      data.ignition_systemd_unit.fstrim_timer.rendered,
       data.ignition_systemd_unit.locksmithd_cfssl.rendered,
       data.ignition_systemd_unit.node-exporter.rendered,
       data.ignition_systemd_unit.node_textfile_inode_fd_count_service.rendered,
