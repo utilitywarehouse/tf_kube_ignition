d809308736e055b7b7d2a42fadb2787ad791423b
DTLP <dlapo@uw.co.uk>
2024-11-12 15:14:36 +0000

    Bump etcd to v3.5.16

--- variables.tf
+++ variables.tf
@@ -64,7 +64,7 @@ variable "etcd_image_url" {
 
 variable "etcd_image_tag" {
   description = "The version of the etcd image to use."
-  default     = "v3.5.12"
+  default     = "v3.5.16"
 }
 
 variable "etcd_data_dir" {
