b79ef9b43fb1ce05e3eeffe2510a1d92775d32f2
DTLP <dlapo@uw.co.uk>
2025-08-05 11:01:50 +0100

    Upd etcd to v3.6.0

--- variables.tf
+++ variables.tf
@@ -64,7 +64,7 @@ variable "etcd_image_url" {
 
 variable "etcd_image_tag" {
   description = "The version of the etcd image to use."
-  default     = "v3.6.4"
+  default     = "v3.6.0"
 }
 
 variable "etcd_data_dir" {
