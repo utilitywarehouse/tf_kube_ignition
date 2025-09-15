de6839f051ba54a5599539937e09048f826574b7
Dmitrijs Lapo <61912981+DTLP@users.noreply.github.com>
2025-09-15 15:08:22 +0100

    Upd Kubernetes to v1.34 (#248)

--- variables.tf
+++ variables.tf
@@ -84,7 +84,7 @@ variable "node_exporter_image_tag" {
 
 variable "kubernetes_version" {
   description = "Kubernetes version, used to specify registry.k8s.io docker image version and Kubernetes binaries"
-  default     = "v1.33.0"
+  default     = "v1.34.0"
 }
 
 variable "cluster_dns" {
