7f981c0b76716a522cc4aff4468a0e1bdd54307e
Foivos Filippopoulos <ffilippopoulos@uw.co.uk>
2026-05-11 14:40:57 +0100

    Update CFSSL NGINX proxy version to v1.30.0 (#259)

--- variables.tf
+++ variables.tf
@@ -261,7 +261,7 @@ variable "dockerhub_password" {
 
 variable "nginx_image" {
   description = "https://github.com/nginx/nginx/releases"
-  default     = "nginx:1.29.4-alpine"
+  default     = "nginx:1.30.0-alpine"
 }
 
 variable "feature_gates" {
